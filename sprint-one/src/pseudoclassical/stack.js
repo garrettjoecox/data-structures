03fe05f48e4ee1d48415e4be68a74e0f22136436
garrettjoecox <garrettjcox@gmail.com>
2015-02-04 15:00:17 -0800

    Implementation of all stack types

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
+  this.length = 0;
+  this.storage = {};
 
+};
+  Stack.prototype.size = function() {
+    return this.length;
+  };
+  Stack.prototype.push = function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  };
+  Stack.prototype.pop = function() {
+    this.length && this.length--;
+    var popped = this.storage[this.length];
+    delete this.storage[this.length];
+    return popped;
+  };
+
+// var someInstance = new Stack();
 
