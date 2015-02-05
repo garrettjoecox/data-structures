91ccb271ba36a81474dc78e56243d53db88adc5f
garrettjoecox <garrettjcox@gmail.com>
2015-02-04 20:42:01 -0800

    implementation of linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,39 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  console.log()
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (!list.tail) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    var oldHeadValue = list.head.value;
+    var newHead = list.head.next;
+
+    list.head = newHead;
+    return oldHeadValue;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    var contains = false;
+    if (list.tail.value === target) return true;
+    while (currentNode.next) {
+      if (currentNode.value === target) {
+        contains = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return contains;
   };
-
   return list;
 };
 
