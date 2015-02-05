f9b1d68c6979d0a19d191a06bffcf931982160f4
garrettjoecox <garrettjcox@gmail.com>
2015-02-05 11:21:52 -0800

    Implementation of Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,51 @@
 
 
 var Graph = function(){
+  this.nodes = [];
+  this.edges = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes.push(node);
 };
 
 Graph.prototype.contains = function(node){
+  return _.contains(this.nodes, node);
 };
 
 Graph.prototype.removeNode = function(node){
+  this.nodes.pop(node);
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var hasEdge = false;
+  if ( _.contains(this.edges[fromNode], toNode) ) {
+    hasEdge = true;
+  }
+  if ( _.contains(this.edges[toNode], fromNode) ) {
+    hasEdge = true;
+  }
+  return hasEdge;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if (!this.edges[fromNode]) {
+    this.edges[fromNode] = [];
+  }
+  if (!this.edges[toNode]) {
+    this.edges[toNode] = [];
+  }
+    this.edges[toNode].push(fromNode);
+    this.edges[fromNode].push(toNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  this.edges[fromNode].pop(toNode);
+  this.edges[toNode].pop(fromNode);
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.nodes, cb);
 };
 
 /*
